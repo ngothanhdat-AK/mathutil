c08fbb1ba33682122fb4d60d4e7c54eaffcf8645
ngothanhdat-AK <ngothanhdat4002@gmail.com>
2025-06-02 15:37:34 +0700

    test1

--- src/main/java/com/thdat/tool/Main.java
+++ src/main/java/com/thdat/tool/Main.java
@@ -7,6 +7,14 @@ import com.thdat.tool.mathutil.core.MathUtility;
 public class Main {
     public static void main(String[] args) {
         testFactorialGivenRightArgument0RunWell();
+        testFactorialGivenRightArgument1RunWell();
+        testFactorialGivenRightArgumentRunWell();
+    }
+    public static void testFactorialGivenRightArgumentRunWell() {
+        System.out.println("2! expected: 2:  + actual: " + MathUtility.getFactorial(2));
+        System.out.println("3! expected: 6:  + actual: " + MathUtility.getFactorial(2));
+        System.out.println("4! expected: 24:  + actual: " + MathUtility.getFactorial(2));
+        System.out.println("5! expected: 120:  + actual: " + MathUtility.getFactorial(2));
     }
     //test ham vua go
     //can co test case
@@ -15,12 +23,26 @@ public class Main {
     //       Call GETFACTORIAL( n = 0 )
     //EXPECTED VALUE: HOPE TO SEE 1 AS THE RESULT OF 0!
     //STATIC: CHO RUN!!
-    public static void testFactorialGivenRightArgument0RunWell(){
+    public static void testFactorialGivenRightArgument0RunWell() {
         int n = 0;
-        long expected = 1 ;
+        long expected = 1;
         long actual = MathUtility.getFactorial(n);
         System.out.println("0! expected: " + expected + "actual: " + actual);
 
+    }
+
+    //test ham vua go
+    //can co test case
+    //testcase 2: check/verify the GETFACTORIAL() METHOD WITH n = 0;
+    //STEPs: Given n = 1;
+    //       Call GETFACTORIAL( n = 0 )
+    //EXPECTED VALUE: HOPE TO SEE 1 AS THE RESULT OF 0!
+    //STATIC: CHO RUN!!
+    public static void testFactorialGivenRightArgument1RunWell() {
+        int n = 1;
+        long expected = 1;
+        long actual = MathUtility.getFactorial(n);
+        System.out.println("1! expected: " + expected + "actual: " + actual);
     }
 }
 
